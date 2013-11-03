5c2b5f50da9a1dd848a099ddc9dfe6b53949fe94
Harrison Powers <fijimunkii@gmail.com>
2013-11-03 15:46:58 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sift::Application.config.secret_token = '8ace66146393ff399605646a5cd6710a157cb8d33973fe919e431ce9a22a1b664ce7519dda22f3ed448e0644da90e03165a5d9be0a6d2ab1cdfc91565a0a23f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sift::Application.config.session_store :cookie_store, key: '_sift_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sift::Application.config.session_store :active_record_store
